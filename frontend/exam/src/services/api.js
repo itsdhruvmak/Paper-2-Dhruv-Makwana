820a94e661b866d3db806d07f95e1fb374d7ebed
itsdhruvmak <129657671+itsdhruvmak@users.noreply.github.com>
2025-12-11 10:24:51 +0530

    Update api.js

--- frontend/exam/src/services/api.js
+++ frontend/exam/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api"
+    baseURL: import.meta.env.VITE_API_URL || "https://paper-2-dhruv-makwana.onrender.com/api"
 })
 
 api.interceptors.request.use((config) => {
